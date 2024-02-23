c1d9f2bbec742859e07af2a523b2b3939b17ecd7
Canoral <kaya68@outlook.com>
2024-02-23 10:00:18 +0100

    ci: :hammer: Installation & Configuration react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={} errorElement={''}>
+      <Route errorElement={}>
+        <Route index element={} />
+
+      </Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+);
