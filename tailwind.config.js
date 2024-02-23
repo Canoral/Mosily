17c867f09e7e08f0419dd943eba4b3bb757e93e6
Canoral <kaya68@outlook.com>
2024-02-23 09:50:04 +0100

    ci: :wrench: Configuration la taille des écrans pour le responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      mobile: { max: "639px" },
+      tablet: { max: "900px" },
+      desktop: { max: "1024px" },
+    },
   },
   plugins: [require("daisyui")],
 
   // daisyUI config (optional - here are the default values)
   daisyui: {
-    themes: ["light + dark"],
+    themes: ["light, dark"],
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
