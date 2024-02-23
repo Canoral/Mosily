55a902b002a4349a4df251dc4706e06bb77e6c74
Canoral <kaya68@outlook.com>
2024-02-23 11:02:56 +0100

    feat: :sparkles: :rocket: Footer est en place!

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer fixed bottom-0 h-16 flex justify-center items-center bg-base-300">
+      © 2024 Mosily™ All Rights Reserved.
+    </footer>
+  );
+}
