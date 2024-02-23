72ff12c2dc929cb74919b90a63d5b57f4bfa617c
Canoral <kaya68@outlook.com>
2024-02-23 17:02:45 +0100

    feat: :sparkles: :rocket: Le composant Alerts est en place

--- src/components/Alertes.tsx
+++ src/components/Alertes.tsx
@@ -1,27 +1,34 @@
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { IAlerts } from "../@types/alertes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Alertes({ alerts }: { alerts: IAlerts[] }) {
-  console.log("alerts :", alerts);
   return (
-    <div className="alerts">
-      <h1 className="text-lg font-bold text-center mb-5">Alertes !</h1>
-      <table className="table">
-        <tbody>
-          {alerts.map((alert) => (
-            <tr key={alert.id}>
-              <th>{alert.id}</th>
-              <td>{alert.alert_text}</td>
-              <td>{alert.alert_formula}</td>
-              {alert.accountsAlerts.length > 0 &&
-                alert.accountsAlerts[0].status === "New" && (
-                  <td className="font-bold badge-primary text-center">
-                    {alert.accountsAlerts[0].status}
-                  </td>
-                )}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <section className="cards">
+      <h1 className="text-lg font-bold text-center pt-2">Alertes !</h1>
+      <div className="flex gap-2 tablet:flex-col tablet:items-center">
+        {alerts.map((alert) => (
+          <div className="card w-2/3 bg-base-100 shadow-xl" key={alert.id}>
+            <div className="card-body">
+              {alert.accountsAlerts[0] &&
+              alert.accountsAlerts[0].status === "New" ? (
+                <div className="badge badge-secondary">NEW</div>
+              ) : (
+                <div className="badge badge-neutral">OLD</div>
+              )}
+              <h2 className="text-md font-bold">{alert.alert_text}</h2>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary btn-sm mr-auto">
+                  Solution
+                </button>
+                <button onClick={() => {}}>
+                  <FontAwesomeIcon icon={faTrashCan} />
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
